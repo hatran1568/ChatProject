5c50bdaa12e423014c96c024d08dafa6579b196e
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-03-27 16:55:37 +0700

    Add room name header

---
+++ WebApplication1/wwwroot/js/Chat.js
@@ -0,0 +1,21 @@
+﻿var renameRoomBtn = document.getElementById('rename-room-btn')
+var renameRoomModal = document.getElementById('rename-room-modal')
+renameRoomBtn.addEventListener('click', function () {
+    renameRoomModal.classList.add('modal-active')
+    event.preventDefault();
+})
+
+function closeRenameModal() {
+    renameRoomModal.classList.remove('modal-active')
+}
+
+var leaveRoomBtn = document.getElementById('leave-room-btn')
+var leaveRoomModal = document.getElementById('leave-room-modal')
+leaveRoomBtn.addEventListener('click', function () {
+    leaveRoomModal.classList.add('modal-active')
+    event.preventDefault();
+})
+
+function closeLeaveModal() {
+    leaveRoomModal.classList.remove('modal-active')
+}
