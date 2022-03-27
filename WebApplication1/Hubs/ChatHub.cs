1c59626fba15c7c27493ac10286f33a0a9a45b75
Tran Thi Nguyet Ha <hattnhe153222@fpt.edu.vn>
2022-03-27 21:20:06 +0700

    Online status

--- WebApplication1/Hubs/ChatHub.cs
+++ WebApplication1/Hubs/ChatHub.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChatProject.Model;
+using ChatProject.ViewModels;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ChatProject.Hubs
@@ -12,5 +15,23 @@ namespace ChatProject.Hubs
         {
             return Context.ConnectionId;
         }
+        public override async Task OnConnectedAsync()
+        {
+            if (!ConnectedUser.Ids.Contains(Context.User.Identity.Name))
+                ConnectedUser.Ids.Add(Context.User.Identity.Name);
+            string json = JsonSerializer.Serialize(ConnectedUser.Ids);
+
+            await Clients.All.SendAsync("UserJoined", json);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            if (ConnectedUser.Ids.Contains(Context.User.Identity.Name))
+                ConnectedUser.Ids.Remove(Context.User.Identity.Name);
+            string json = JsonSerializer.Serialize(ConnectedUser.Ids);
+
+            await Clients.All.SendAsync("UserJoined", json);
+        }
+
     }
 }
